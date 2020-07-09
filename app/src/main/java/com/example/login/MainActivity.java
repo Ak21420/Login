5c433d1e0bc81be80c7403f1e369274c47d1b106
akshat214 <akshat21420shah@gmail.com>
2020-07-09 12:15:50 +0530

    first demo commit

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private  Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+    }
+
+}
